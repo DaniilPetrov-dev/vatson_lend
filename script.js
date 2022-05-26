2479a8f5e54aeff85c3832d35aefd66d6d43aba5
DaniilPetrov-dev <ichitrigerfingern1ggas@gmail.com>
2022-05-27 00:08:22 +0300

    Fifth commit. Make slider script, header links.

---
+++ script.js
@@ -0,0 +1,67 @@
+var slideNow = 1;
+var slideCount = $('#slidewrapper').children().length;
+var translateWidth = 0;
+var slideInterval = 2500;
+var navBtnId = 0;
+
+function prevSlide() {
+    if (slideNow == 1 || slideNow <= 0 || slideNow > slideCount) {
+        translateWidth = -$('#viewport').width() * (slideCount - 1);
+        $('#slidewrapper').css({
+            'transform': 'translate(' + translateWidth + 'px, 0)',
+            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+        });
+        slideNow = slideCount;
+    } else {
+        translateWidth = -$('#viewport').width() * (slideNow - 2);
+        $('#slidewrapper').css({
+            'transform': 'translate(' + translateWidth + 'px, 0)',
+            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+        });
+        slideNow--;
+    }
+}
+
+function nextSlide() {
+    if (slideNow == slideCount || slideNow <= 0 || slideNow > slideCount) {
+        $('#slidewrapper').css('transform', 'translate(0, 0)');
+        slideNow = 1;
+    } else {
+        translateWidth = -$('#viewport').width() * (slideNow);
+        $('#slidewrapper').css({
+            'transform': 'translate(' + translateWidth + 'px, 0)',
+            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+        });
+        slideNow++;
+    }
+}
+
+function contactsAlert() {
+	alert('А котнтакты спиздили');
+}
+
+
+$(document).ready(function () {
+    var switchInterval = setInterval(nextSlide, slideInterval);
+
+    $('#viewport').hover(function(){
+        clearInterval(switchInterval);
+    },function() {
+        switchInterval = setInterval(nextSlide, slideInterval);
+    });
+
+	$('#next-btn').click(function() {
+        nextSlide();
+    });
+
+    $('#prev-btn').click(function() {
+        prevSlide();
+	});
+
+	$('#contacts').click(function() {
+		contactsAlert();
+	});
+});
